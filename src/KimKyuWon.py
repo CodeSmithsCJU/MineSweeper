3e1e2d5267e4064e94f398e8e5097d8fd665f348
kene0069 <kimonetwokw@gmail.com>
2025-11-14 20:22:12 +0900

    구구단 게임

--- src/KimKyuWon.py
+++ src/KimKyuWon.py
@@ -1,3 +1,17 @@
-print("Hi?")
-print("hh")
-print("123")
+import random
+print("구구단 게임~!")
+print("랜덤한 구구단이 3번 나올거에요~ 한번 자알 맞춰보세요~")
+count = 0
+for i in range(3):
+    x = random.randrange(1,10)
+    y = random.randrange(1,10)
+    s = x*y
+    ans = int(input(f"{x} X {y} ="))
+
+    if ans == s:
+        print("정답입니다~")
+    else:
+        print(f"틀렸습니다.. \n정답은:{s} 입니다..")
+        count += 1
+
+print(f"끝났습니다~ \n틀린 개수는 {count}개 입니다.")
