3d35438738055409790d467edb07667b84da965b
jaehwan24601 <jaehwan8984@gmail.com>
2025-11-14 20:36:58 +0900

    ver2

--- src/Jaehwan.py
+++ src/Jaehwan.py
@@ -1,6 +1,14 @@
-i = 0
+import random
+i=0
 n=0
 lst = ["me", "you", "kim", "seong"]
+print("홀짝게임 시작 1, 2중 선택")
+
 while n < len(lst):
-    print(lst[n], "is babo")
+    r = random.randrange(1,3)
+    ran = int(input("숫자 입력: "))
+    if ran == r:
+        print(lst[n], "is babo")
+    else:
+        print("you are lucky")
     n=n+1
