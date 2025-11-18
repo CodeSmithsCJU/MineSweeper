8f793d74d1bb92ace80945d78b4e8c1a698c21e5
jaehwan24601 <jaehwan8984@gmail.com>
2025-11-18 20:30:44 +0900

    Main2

--- src/Main2.py
+++ src/Main2.py
@@ -1,21 +1,36 @@
+#mine = -1
+#flag = -2
+#답지는 폭탄 -1과 숫자로 써져있는 완성된 배열
+#풀이는 연산 시행 부분 대조한 뒤 가져오기
 def leftclick():
-    if(board_open[x][y] == True):
-        #낫띵
+    if(b_open[x][y] == True):
+        #낫띵해쁜드
         return
-    if(board_close[x][y] == True):
+    if(b_close[x][y] == True):
+        if():
+            return
+        if():
+            return
+        if():
+            return
+        if():
+            return
+        if():
+            return
         #재귀저기연산 시작
         return
-    if(board[x][y]==mine):
+    if(b[x][y] == -1):
         #calling gameover 
         return
     
 def rightclick():
-    if(board[x][y]==nonclick):
-        #깃발 생성
+    if(b[x][y]==nonclick):
+        #깃발 이미지로 변경
         return
-    if(board[x][y]==flag):
-        #깃발 제거
+    if(b[x][y] == -2):
+        #칸 기본 이미지로 변경
         return
-    if(board[x][y]==mine):
-        #깃발과 지뢰의 수가 동일한지? 동일하면 승리
-        return
+    if(b[x][y] == -1):
+        if(-1 == -2):
+            #게임 승리 불러오기
+            return
