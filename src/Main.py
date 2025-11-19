9b3630c9048e4e525f95a72cfcf89cc550597953
jaehwan24601 <jaehwan8984@gmail.com>
2025-11-19 20:21:51 +0900

    ij

--- src/Main.py
+++ src/Main.py
@@ -7,51 +7,51 @@
 arr = [(-1, -1), (-1, 0), (-1, 1),( 0, -1), ( 0, 1),( 1, -1), ( 1, 0), ( 1, 1)]
 #답지는 폭탄 -1과 숫자로 써져있는 완성된 배열
 #풀이는 연산 시행 부분 대조한 뒤 가져오기
-def leftclick(j,i):
-    if b2[j][i] != NaN:  #낫띵해쁜드
+def leftclick(i,j):
+    if b2[i][j] != NaN:  #낫띵해쁜드
         return
     
     else:
-        if b2[j][i] == -2:
+        if b2[i][j] == -2:
             # 깃발 
             return
-        if 1 <= b1[j][i] and b1[j][i] <= 8:#답지 칸 번호 배열 불러오기
-            b2[j][i] = b1[j][i]
+        if 1 <= b1[i][j] and b1[i][j] <= 8:#답지 칸 번호 배열 불러오기
+            b2[i][j] = b1[i][j]
             return
-        if b1[j][i] == -1:
+        if b1[i][j] == -1:
             clear = 2 
             return
-        if b1[j][i] == 0:
-            repeat(j,i)
+        if b1[i][j] == 0:
+            repeat(i,j)
             return
         return
 
-def repeat(j,i): #좌클했는데 0일경우
+def repeat(i,j): #좌클했는데 0일경우
     try:
-        if b2[j][i] != NaN: #만약 열려있는 칸이라면 건너뜀
+        if b2[i][j] != NaN: #만약 열려있는 칸이라면 건너뜀
             return
         
-        b2[j][i] = b1[j][i] #닫혀있는 칸을 열어줌
+        b2[i][j] = b1[i][j] #닫혀있는 칸을 열어줌
 
-        if 1 <= b1[j][i] and b1[j][i] <= 8: #연 칸이 숫자라면 종료
+        if 1 <= b1[i][j] and b1[i][j] <= 8: #연 칸이 숫자라면 종료
             return
 
         for k in range(8): #0인 칸만 남으면 재귀 ㄱㄱ
-            jj = j + arr[k][0]
-            ii = i + arr[k][1]
-            repeat(jj,ii)
+            ii = i + arr[k][0]
+            jj = j + arr[k][1]
+            repeat(ii,jj)
 
     except:
         return #테두리 칸 그거 어쩌구
     return
 
 def rightclick(): #우클
-    if b2[j][i] == NaN:
-        b2[j][i] = -2
+    if b2[i][j] == NaN:
+        b2[i][j] = -2
         #깃발 이미지로 변경
         return
-    if b2[j][i] == -2:
-        b2[j][i] == NaN
+    if b2[i][j] == -2:
+        b2[i][j] == NaN
         #칸 기본 이미지로 변경
         return
 
